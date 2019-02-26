fac98b200f6ba5f4c48b32a68933ae395eec4acd
Max Goodman <max.goodman@flatironschool.com>
2019-02-26 17:47:11 -0500

    Adding first two red-green tests

---
+++ click-counter/src/App.test.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Enzyme, {shallow} from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
+import App from './App';
+
+Enzyme.configure({adapter: new EnzymeAdapter() });
+
+const setup = (props={}, state=null) =>{
+  return shallow(<App {...props} />)
+} //setup app to take props and state 
+
+test('renders without crashing', () => {
+  const wrapper = shallow(<App />); //data-test attribute
+  const appComponent = wrapper.find("[data-test='component-app']")
+  expect(appComponent.length).toBe(1);
+});
+
+test('renders increment button', ()=>{
+  const wrapper = shallow(<App />);
+  const appComponent = wrapper.find("[data-test='increment-button']")
+  expect(appComponent.length).toBe(1);
+});
+
+test('renders counter display', ()=>{
+  const wrapper = shallow(<App />);
+  const appComponent = wrapper.find("[data-test='counter-display']")
+  expect(appComponent.length).toBe(1);
+});
+
+test('counter starts at 0', ()=>{
+
+});
+
+test('clicking button increments counter display', ()=>{
+
+});
